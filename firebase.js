5ae72d957deed2c5d2125569ace9c7a122052b36
AkshayPatil0 <60181175+AkshayPatil0@users.noreply.github.com>
2020-08-13 11:51:26 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDACEJ36mIEOB5d2Vp7iVqG3gM0Kieja4w",
-    authDomain: "farmer-app-54614.firebaseapp.com",
-    databaseURL: "https://farmer-app-54614.firebaseio.com",
-    projectId: "farmer-app-54614",
-    storageBucket: "farmer-app-54614.appspot.com",
-    messagingSenderId: "672812562257",
-    appId: "1:672812562257:web:afc9656889303b00ef4570",
-    measurementId: "G-3Y4LV388F7"
+    //firebase config
   };
   
 export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
